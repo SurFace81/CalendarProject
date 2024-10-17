044c970da5768a4373ba6925c7bb35d6e4141857
Frouloss <149324378+Frouloss@users.noreply.github.com>
2024-10-17 22:34:40 +0300

    Создал профиль юзера, вместо аватарки пока заглушка

--- CalendarProject/Views/UserProfilePage.xaml.cs
+++ CalendarProject/Views/UserProfilePage.xaml.cs
@@ -1,8 +1,14 @@
 ﻿using CalendarProject.ViewModels;
-
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 namespace CalendarProject.Views;
+
 
 public sealed partial class UserProfilePage : Page
 {
@@ -15,5 +21,20 @@ public sealed partial class UserProfilePage : Page
     {
         ViewModel = App.GetService<UserProfileViewModel>();
         InitializeComponent();
+    }
+
+    private void ChangeAvatarButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Нужно сделать добавление картинки
+    }
+
+    private void SaveButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Пустой обработчик для кнопки сохранения информации
+    }
+
+    private void LogoutButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Пустой обработчик для кнопки выхода из аккаунта
     }
 }
